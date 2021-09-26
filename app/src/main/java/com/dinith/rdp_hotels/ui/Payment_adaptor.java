1ae218d710778f14b8a2a7f1f1c932a6d8f99f08
pula94 <l.pulasthiperera@outlook.com>
2021-09-26 16:06:24 +0530

    add payment

---
+++ app/src/main/java/com/dinith/rdp_hotels/ui/Payment_adaptor.java
@@ -0,0 +1,111 @@
+package com.dinith.rdp_hotels.ui.home;
+
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dinith.rdp_hotels.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter  extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private Context mContext;
+    private List<Upload> mUploads;
+    private ImageAdapter.OnItemClickListener mListener;
+
+    public ImageAdapter(Context context, List<Upload> uploads) {
+        mContext = context;
+        mUploads = uploads;
+    }
+
+    @Override
+    public ImageAdapter.ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.roomview, parent, false);
+        return new ImageAdapter.ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageAdapter.ImageViewHolder holder, int position) {
+
+
+        Upload uploadCurrent = mUploads.get(position);
+        //  String palce_name = uploadCurrent.getNameOfPlace();
+
+        // holder.textViewName.setText(palce_name);
+        Picasso.get()
+                .load(uploadCurrent.getImageUrl())
+                .placeholder(R.mipmap.ic_launcher)
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+            View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
+        public TextView textViewName;
+        public ImageView imageView;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.text);
+            imageView = itemView.findViewById(R.id.imageview);
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
+
+
+
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    mListener.onItemClick(position);
+                }
+            }
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+
+            return false;
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+
+
+    }
+
+    public void setOnItemClickListener(ImageAdapter.OnItemClickListener listener) {
+        mListener = listener;
+    }
+}
