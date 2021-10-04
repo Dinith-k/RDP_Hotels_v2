e0cd92eb490257a47c528a114d044dbdaddc0a8e
Dinith Kaushalya <dinithkaushalya55@gmail.com>
2021-10-04 10:51:39 +0530

    logo

--- app/src/main/java/com/dinith/rdp_hotels/ui/home/HomeFragment.java
+++ app/src/main/java/com/dinith/rdp_hotels/ui/home/HomeFragment.java
@@ -1,20 +1,27 @@
 package com.dinith.rdp_hotels.ui.home;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.dinith.rdp_hotels.R;
+import com.dinith.rdp_hotels.ui.admin.rooms;
+import com.dinith.rdp_hotels.ui.checkout.checkout;
+import com.dinith.rdp_hotels.ui.menu.menu;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,6 +38,7 @@ import java.util.List;
 public class HomeFragment extends  Fragment  {
 
    ImageView profile;
+   ImageButton reservation,menu,checkin,checkout;
    TextView name;
     private DatabaseReference mDatabaseRef;
     @Nullable
@@ -41,6 +49,52 @@ public class HomeFragment extends  Fragment  {
 
         profile = (ImageView)view.findViewById(R.id.profile);
         name= (TextView)view.findViewById(R.id.name);
+
+        reservation = (ImageButton) view.findViewById(R.id.reservation);
+        menu = (ImageButton) view.findViewById(R.id.menu);
+        checkin = (ImageButton) view.findViewById(R.id.checkin);
+        checkout = (ImageButton) view.findViewById(R.id.checkout);
+
+
+        reservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                  navController.navigate(R.id.navigation_dashboard);
+            }
+        });
+
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), com.dinith.rdp_hotels.ui.menu.menu.class);
+                startActivity(intent);
+            }
+        });
+
+
+        checkin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), com.dinith.rdp_hotels.ui.menu.menu.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), com.dinith.rdp_hotels.ui.checkout.checkout.class);
+                startActivity(intent);
+
+            }
+        });
+
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
